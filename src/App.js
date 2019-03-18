3b250c471c7b92e7bfff6edd40790c81d2088040
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2019-03-18 18:47:09 -0400

    YTSearch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/searchBar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = "AIzaSyBBMHsp2o9y-LybYuLwYMaE-afeLOnYHRw";
+
+YTSearch({key: API_KEY, term: 'Lambda School' }, function(data) {
+  console.log(data);
+});
 
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
-const API_KEY = "AIzaSyBBMHsp2o9y-LybYuLwYMaE-afeLOnYHRw";
-
-
 ReactDOM.render(<App />, 
 document.getElementById('root'));
 
