b5f227fc13d61b6457c290e399663e179c27616c
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2019-03-18 17:34:49 -0400

    SearchBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './components/searchBar';
+
+const API_KEY = "AIzaSyBBMHsp2o9y-LybYuLwYMaE-afeLOnYHRw";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <SearchBar />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const API_KEY = "AIzaSyBBMHsp2o9y-LybYuLwYMaE-afeLOnYHRw";
+
+
+ReactDOM.render(<App />, 
+document.getElementById('root'));
 
 
