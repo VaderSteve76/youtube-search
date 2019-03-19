8146e8bdf87299b1f0e3f3683c2079cf6e933562
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2019-03-19 16:46:50 -0400

    Images & Title Rendering

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/searchBar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/videoList';
 
 const API_KEY = "AIzaSyBBMHsp2o9y-LybYuLwYMaE-afeLOnYHRw";
 
-YTSearch({key: API_KEY, term: 'Lambda School' }, function(data) {
-  console.log(data);
-});
-
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+    YTSearch({key: API_KEY, term: 'VS Code' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
