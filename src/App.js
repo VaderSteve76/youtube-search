499e4505e453be158675dd6997fdeb484b58bfe3
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2019-03-19 18:49:34 -0400

    Search Working

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import './App.css';
 import SearchBar from './components/searchBar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/videoList';
 import VideoDetail from './components/videoDetail';
 
-const API_KEY = "AIzaSyBBMHsp2o9y-LybYuLwYMaE-afeLOnYHRw";
+const API_KEY = "AIzaSyAQPgviXaIWsadOMzPEai-vYj-CJvNLRlA";
+
+// AIzaSyBBMHsp2o9y-LybYuLwYMaE-afeLOnYHRw
+// AIzaSyAQPgviXaIWsadOMzPEai-vYj-CJvNLRlA
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     };
-    YTSearch({key: API_KEY, term: 'VS Code' }, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('VS Code');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term }, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 400)
+
     return (
       <div className="App">
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} 
+        />
       </div>
     );
   }
